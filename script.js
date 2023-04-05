4859d9203e6354d403156e6cc00ae63b9a990e13
JoseArielGimenezVargas <arielgimenezvargas@gmail.com>
2023-04-05 00:41:55 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function consultar(){
+
+    let email = document.getElementsByClassName("consulta");
+    prompt("Ingrese su correo electrónico");
+  alert("Gracias. Un asesor se contactará con Usted.");
+}
+
